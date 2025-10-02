c32dceafdeef5dcfae3c94026e6122e13415449d
Ali Raza <Ali@Verokes-MacBook-Pro.local>
2025-10-02 15:41:53 +0500

    Splash screen is implemented

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.hilt) // ✅ Hilt plugin from version catalog
+    kotlin("kapt")           // ✅ Needed for annotation processing
 }
 
 android {
@@ -27,6 +29,7 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -39,8 +42,12 @@ android {
     }
 }
 
-dependencies {
+hilt {
+    enableAggregatingTask = false
+}
 
+dependencies {
+    // AndroidX + Compose (from version catalog)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -49,6 +56,9 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+//    implementation(libs.androidx.material3)
+
+    // Testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -56,4 +66,27 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    // ✅ Dagger Hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+
+    // ✅ AndroidX Hilt
+    implementation(libs.androidx.hilt.navigation.compose)
+    kapt(libs.androidx.hilt.compiler)
+
+    implementation(libs.androidx.datastore.preferences)
+    implementation(libs.androidx.datastore.core)
+
+    implementation("androidx.room:room-runtime:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+
+// Kotlin Extensions and Coroutines support
+    implementation("androidx.room:room-ktx:2.6.1")
+
+    implementation("androidx.compose.material:material:1.6.8")
+
+    implementation("com.google.accompanist:accompanist-pager:0.36.0")
+    implementation("com.google.accompanist:accompanist-pager-indicators:0.36.0")
+
 }
---
+++ app/src/main/java/com/alidev/ecommerceapplication/navigation/RootNavigationGraph.kt
@@ -0,0 +1,30 @@
+package com.alidev.ecommerceapplication.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.alidev.ecommerceapplication.navigation.screen.Screen
+import com.alidev.ecommerceapplication.presentation.SplashScreen
+
+@Composable
+fun RootNavigationGraph(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        route = Graph.ROOT,
+        startDestination = Screen.Splash.route
+    ) {
+
+        composable(route = Screen.Splash.route) {
+            SplashScreen(navController = navController)
+        }
+//
+//        composable(route = Screen.OnBoarding.route) {
+//            OnBoardingScreen(navController = navController)
+//        }
+//
+//        composable(route = Graph.MAIN) {
+//            MainScreen()
+//        }
+    }
+}
